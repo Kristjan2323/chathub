3c41aa5fa887ad14fb9849ec0636c8fb37835289
Kristi Ballolli <kristianballolli1@gmail.com>
2023-10-21 20:12:37 +0200

    chatHub | React.JS + SignalR

---
+++ src/Components/ContactList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactList(){
+
+    return(
+        <section>
+            <p>ContactList</p>
+        </section>
+    )
+}
---
+++ src/Components/Layout/ConversationChat.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactList(){
+
+    return(
+        <section>
+            <p>hello from converationList</p>
+        </section>
+    )
+}
